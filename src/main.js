982f3ac6524425e64a70951f7c081b7de04478b1
ynnckth <yannickstreit@hotmail.com>
2023-04-12 22:51:12 +0800

    Listen to touch events to control player by swiping

--- src/main.js
+++ src/main.js
@@ -1,18 +1,60 @@
-import { LEVELS, arrowKeyCodes } from "./constants.js";
+import { arrowKeyCodes, LEVELS } from "./constants.js";
 import { Level } from "./Level.js";
 import { DOMDisplay } from "./DOMDisplay.js";
 
 const trackKeys = (arrowKeyCodes) => {
   const pressed = Object.create(null);
+  let xDown = null;
+  let yDown = null;
+  let xUp = null;
+  let yUp = null;
+  let xDiff = null;
+  let yDiff = null;
+
+  const handleTouchStart = (event) => {
+    const firstTouch = (event.touches || event.originalEvent.touches)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  };
+
+  const handleTouchMove = (event) => {
+    if (!xDown || !yDown) return;
+    xUp = event.touches[0].clientX;
+    yUp = event.touches[0].clientY;
+    xDiff = xDown - xUp;
+    yDiff = yDown - yUp;
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+        pressed["left"] = true;
+      } else {
+        pressed["right"] = true;
+      }
+    } else {
+      if (yDiff > 0) {
+        pressed["up"] = true;
+      }
+    }
+  };
+
+  const handleTouchEnd = () => {
+    xDown = null;
+    yDown = null;
+    pressed["right"] = false;
+    pressed["left"] = false;
+    pressed["up"] = false;
+  };
+
   const handler = (event) => {
     if (arrowKeyCodes.hasOwnProperty(event.keyCode)) {
-      const down = event.type === "keydown";
-      pressed[arrowKeyCodes[event.keyCode]] = down;
+      pressed[arrowKeyCodes[event.keyCode]] = event.type === "keydown";
       event.preventDefault();
     }
   };
   addEventListener("keydown", handler);
   addEventListener("keyup", handler);
+  addEventListener("touchstart", handleTouchStart);
+  addEventListener("touchmove", handleTouchMove);
+  addEventListener("touchend", handleTouchEnd);
   return pressed;
 };
 
